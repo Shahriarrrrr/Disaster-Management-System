f43f8af0243fffb20b78fefc3b9b824dc0eac930
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-04-24 11:24:57 +0600

    StartingOfDonation

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -7,7 +7,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model  = CustomUser
-        fields = ['user_name','password']
+        fields = ['user_name','password','user_address']
 
     def create(self, validated_data):
         password = validated_data.pop('password')  # Extract password
